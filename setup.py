94c56b58b2822d26114db34371e05e93a5249a75
lamalice25 <titijiyan974@gmail.com>
2024-04-30 23:21:15 +0400

    V1

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 name = "lamalice25"
 version = "0.0.1"
-authors = "lamateam2.0@gmail.com"
-    
-
 description = "A small example package"
 readme = "RTFM.md"
 Homepage = "https://github.com/lamalice25/tutobot-.git"
